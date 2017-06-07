1d798441806da9b469927aafbde962cec0b271d7
Kimberly Pennington <email@kimberly.codes>
2017-06-07 15:37:21 -0600

    flake8 2 blank lines

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,10 +9,12 @@ def pytest_html_results_table_header(cells):
     cells.insert(2, html.th('Description'))
     cells.insert(0, html.th('Time', class_='sortable time', col='time'))
 
+
 @pytest.mark.optionalhook
 def pytest_html_results_table_row(report, cells):
     cells.insert(2, html.td(report.description))
     cells.insert(1, html.td(datetime.utcnow(), class_='col-time'))
+
 
 @pytest.mark.hookwrapper
 def pytest_runtest_makereport(item, call):
