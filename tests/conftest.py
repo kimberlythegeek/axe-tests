f6b77fd074f8955f0365c7e87f771e00f8de1189
Kimberly Pennington <email@kimberly.codes>
2017-06-08 16:13:52 -0600

    removed json link from html report

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 from py.xml import html
 import pytest
-import json
+# import json
 
 
 @pytest.mark.optionalhook
@@ -18,18 +18,7 @@ def pytest_html_results_table_row(report, cells):
 
 @pytest.mark.hookwrapper
 def pytest_runtest_makereport(item, call):
-    # load pytest-html plguin
-    pytest_html = item.config.pluginmanager.getplugin('html')
     outcome = yield
     report = outcome.get_result()
     # add docstring to 'description' column
     report.description = str(item.function.__doc__)
-
-    extra = getattr(report, 'extra', [])
-    xfail = hasattr(report, 'wasxfail')
-    if (report.skipped and xfail) or (report.failed and not xfail):
-        # only add additional html on failure
-        data = json.load(open('result.json'))
-        # add the violations JSON object to h
-        extra.append(pytest_html.extras.text(json.dumps(data['violations'], indent=4)))
-    report.extra = extra
